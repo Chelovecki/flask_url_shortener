18372567d2e0166fa40c7eb5d2b114d518196870
Benjamin Kozlov Laptop <i@benjamen.ru>
2025-11-11 00:15:54 +0300

    decompose funtions + hide details on class

--- main.py
+++ main.py
@@ -5,79 +5,84 @@ app = Flask(__name__)
 
 
 class URLShortener:
-    symbols_for_generate_short_link = string.digits + string.ascii_lowercase
-    urls_data = {}
-
     def __init__(self):
-        pass
+        self.symbols_for_short_link = string.digits + string.ascii_lowercase
+        self.urls_data = {}
 
-    @classmethod
-    def get_rand_link(cls):
-
-        url = "".join([random.choice(cls.symbols_for_generate_short_link)
+    def _generate_short_code(self):
+        url = "".join([random.choice(self.symbols_for_short_link)
                       for _ in range(6)])
         return url
 
-    @classmethod
-    def aboba(cls, user_url):
-        unique_code = cls.get_rand_link()
+    def _get_unique_code(self, user_url):
+        unique_code = self._generate_short_code()
+
         yet_existed = None
-        for code, url in cls.urls_data.items():
+        for code, url in self.urls_data.items():
             if url == user_url:
                 yet_existed = code
                 break
-        if yet_existed:
-            unique_code = yet_existed
 
-        cls.set_user_link(user_link=user_url, code=unique_code)
+        if not yet_existed:
+            yet_existed = unique_code
 
-        abs_url = url_for(
+        return yet_existed
+
+    def get_short_link(self, user_url):
+        unique_code = self._get_unique_code(user_url)
+
+        self.set_user_link(user_link=user_url, code=unique_code)
+
+        abs_url_with_short_link = url_for(
             'redirect_to', unique_code=unique_code, _external=True)
 
-        return abs_url
+        return abs_url_with_short_link
 
-    @classmethod
-    def set_user_link(cls, user_link, code):
-        cls.urls_data[code] = user_link
+    def get_user_link(self, short_code):
+        return self.urls_data.get(short_code)
+
+    def set_user_link(self, user_link, code):
+        self.urls_data[code] = user_link
 
 
 @app.route('/urls')
 def urls():
-    return o.urls_data
+    return handler.urls_data
 
 
 @app.route('/redirect/<string:unique_code>', methods=['GET'])
 def redirect_to(unique_code):
     if request.method == 'GET':
-        url_to_redirect = o.urls_data.get(unique_code)
+        url_to_redirect = handler.urls_data.get(unique_code)
 
         return redirect(url_to_redirect)
 
 
 @app.route('/set', methods=['POST', 'GET'])
-def hello():
+def main():
     error = None
     shortened_link = None
     user_link = None
+
     if request.method == 'POST':
         user_link = request.form.get('user_url')
-        print(user_link)
 
+        # пустая ссылка
         if not user_link or user_link.strip() == '':
             error = 'Пожалуйста, введите ссылку'
-        elif (user_link.startswith('http://') + user_link.startswith('https://')) == 0:
+
+        # начинается не с http - не ссылка
+        elif not user_link.startswith('http://') and not user_link.startswith('https://'):
             error = 'Ссылка должна начинаться с http:// или https://'
+
+        # все хорошо, обрабатываем полученную ссылку
         else:
-
-            shortened_link = o.aboba(user_link)
-
-            print(f"получили абсолютный путь: {shortened_link}")
-            print('dict is:', o.urls_data)
-        print(error)
-        return render_template('index.html', orig_link=user_link, shortened_link=shortened_link, error=error)
+            shortened_link = handler.get_short_link(user_link)
+    print(error)
+    return render_template('index.html', orig_link=user_link, shortened_link=shortened_link, error=error)
 
 
 if __name__ == '__main__':
-    o = URLShortener()
+    handler = URLShortener()
 
     app.run(debug=True)
